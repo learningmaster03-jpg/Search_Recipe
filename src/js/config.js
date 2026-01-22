89e54cf2f80932838ad557b70c35248bcfb26316
learningmaster03 <164023805+weblearn24@users.noreply.github.com>
2026-01-22 16:45:50 +0530

    Initial commit for SearchRecipe project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e6c30358-40ec-469a-bb77-92149bea9fd3';
+export const MODAL_CLOSE_SEC = 2.5;
